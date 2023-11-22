b4b34e3f781a97b80433c63ce3a046a2e0c26379
bokisups <bokisups@gmail.com>
2023-11-22 21:57:47 +0100

    Create smallprogs.py

---
+++ python/smallprogs.py
@@ -0,0 +1,17 @@
+#print("Program starting-FizBuzz")
+
+#value = int(input("Enter number from 1 to 100: "))
+
+#for i in range (1,value):
+ #   if i % 3 == 0 and i % 5 == 0:
+  #      print("FizzBuzz")
+   # elif i % 5 ==0:
+    #    print("Buzz")
+    #elif i % 3 ==0:
+     #   print("Fizz")
+    #else: 
+     #   print(i)
+
+#zvrljotina = "Vseckaj pyThoN in HTML in CSS"
+#print(zvrljotina.lower())
+
