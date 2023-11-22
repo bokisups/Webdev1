61d143e36bcb3bc5d5b88cb49e7142522b1e3745
bokisups <bokisups@gmail.com>
2023-11-22 21:57:43 +0100

    Create sample.py

---
+++ python/sample.py
@@ -0,0 +1,31 @@
+#age = int(input("Your age? "))
+
+#if age > 12:
+ #   if age < 20:
+ #       print("Teen")
+#print("End")
+
+#if age >12 and age < 20:
+#    print("Teen")
+#print("End")
+
+#name = input("Your name")
+#if name =="Tina" or name == "TINA":
+#    print("Hello Tina!")
+
+#print("Ziv`. Vtipkaj kilometre za milje")
+
+#while True:
+   # km = float(input("km"))
+
+#    miles = km * 0.76
+
+  #  print(f"{km} je,{miles} milj.")
+
+   # choice = input("Ali zelite nadaljevati? ")
+   # if choice.lower() != "y" and choice.lower != "yes":
+   #     break
+
+
+
+
